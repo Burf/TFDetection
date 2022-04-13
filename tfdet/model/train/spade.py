459834bf410e5a42aea2ca5d5c0d856e9b67a3a5
Hyungjin Kim <flslzk@gmail.com>
2022-04-13 21:19:56 +0900

    replace blank
    replace blank

--- tfdet/model/train/spade.py
+++ tfdet/model/train/spade.py
@@ -3,5 +3,5 @@ import tensorflow as tf
 from ..head.spade import FeatureExtractor
 	
 def train(feature, sampling_index = None):
-	feature = FeatureExtractor(sampling_index = sampling_index, name = "feature_extractor")(feature)
-	return feature
+    feature = FeatureExtractor(sampling_index = sampling_index, name = "feature_extractor")(feature)
+    return feature
