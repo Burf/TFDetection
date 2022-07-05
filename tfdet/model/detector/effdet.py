1693bf0694fac716a835b59920f31fce5b0d227a
Hyungjin Kim <flslzk@gmail.com>
2022-07-05 20:55:36 +0900

    update doc string for default image shape
    update doc string for default image shape

--- tfdet/model/detector/effdet.py
+++ tfdet/model/detector/effdet.py
@@ -29,7 +29,9 @@ def effdet_d0(x, n_class = 21,
               scale = [32, 64, 128, 256, 512], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
               neck = functools.partial(neck, n_feature = 64, neck_n_depth = 3, neck = functools.partial(bifpn, n_feature = 64)),
               convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.swish):
-    #image_shape = [512, 512]
+    """
+    The recommended image shape is 512.
+    """
     feature = effnet_b0(x, weights = weights, indices = [-3, -2, -1])
     out = effdet(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                  scale = scale, ratio = ratio, octave = octave,
@@ -41,7 +43,9 @@ def effdet_d1(x, n_class = 21,
               scale = [32, 64, 128, 256, 512], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
               neck = functools.partial(neck, n_feature = 88, neck_n_depth = 4, neck = functools.partial(bifpn, n_feature = 88)),
               convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.swish):
-    #image_shape = [640, 640]
+    """
+    The recommended image shape is 640.
+    """
     feature = effnet_b1(x, weights = weights, indices = [-3, -2, -1])
     out = effdet(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                  scale = scale, ratio = ratio, octave = octave,
@@ -53,7 +57,9 @@ def effdet_d2(x, n_class = 21,
               scale = [32, 64, 128, 256, 512], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
               neck = functools.partial(neck, n_feature = 112, neck_n_depth = 5, neck = functools.partial(bifpn, n_feature = 112)),
               convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.swish):
-    #image_shape = [768, 768]
+    """
+    The recommended image shape is 768.
+    """
     feature = effnet_b2(x, weights = weights, indices = [-3, -2, -1])
     out = effdet(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                  scale = scale, ratio = ratio, octave = octave,
@@ -65,7 +71,9 @@ def effdet_d3(x, n_class = 21,
               scale = [32, 64, 128, 256, 512], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
               neck = functools.partial(neck, n_feature = 160, neck_n_depth = 6, neck = functools.partial(bifpn, n_feature = 160)),
               convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.swish):
-    #image_shape = [896, 896]
+    """
+    The recommended image shape is 896.
+    """
     feature = effnet_b3(x, weights = weights, indices = [-3, -2, -1])
     out = effdet(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                  scale = scale, ratio = ratio, octave = octave,
@@ -77,7 +85,9 @@ def effdet_d4(x, n_class = 21,
               scale = [32, 64, 128, 256, 512], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
               neck = functools.partial(neck, n_feature = 224, neck_n_depth = 7, neck = functools.partial(bifpn, n_feature = 224)),
               convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.swish):
-    #image_shape = [1024, 1024]
+    """
+    The recommended image shape is 1024.
+    """
     feature = effnet_b4(x, weights = weights, indices = [-3, -2, -1])
     out = effdet(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                  scale = scale, ratio = ratio, octave = octave,
@@ -89,7 +99,9 @@ def effdet_d5(x, n_class = 21,
               scale = [32, 64, 128, 256, 512], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
               neck = functools.partial(neck, n_feature = 288, neck_n_depth = 7, neck = functools.partial(bifpn, n_feature = 288)),
               convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.swish):
-    #image_shape = [1280, 1280]
+    """
+    The recommended image shape is 1280.
+    """
     feature = effnet_b5(x, weights = weights, indices = [-3, -2, -1])
     out = effdet(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                  scale = scale, ratio = ratio, octave = octave,
@@ -101,7 +113,9 @@ def effdet_d6(x, n_class = 21,
               scale = [32, 64, 128, 256, 512], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
               neck = functools.partial(neck, n_feature = 384, neck_n_depth = 8, neck = functools.partial(bifpn, n_feature = 384, weighted_add = False)),
               convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.swish):
-    #image_shape = [1280, 1280]
+    """
+    The recommended image shape is 1280.
+    """
     feature = effnet_b6(x, weights = weights, indices = [-3, -2, -1])
     out = effdet(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                  scale = scale, ratio = ratio, octave = octave,
@@ -113,7 +127,9 @@ def effdet_d7(x, n_class = 21,
               scale = [40, 80, 160, 320, 640], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
               neck = functools.partial(neck, n_feature = 384, neck_n_depth = 8, neck = functools.partial(bifpn, n_feature = 384, weighted_add = False)),
               convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.swish):
-    #image_shape = [1536, 1536]
+    """
+    The recommended image shape is 1536.
+    """
     feature = effnet_b6(x, weights = weights, indices = [-3, -2, -1])
     out = effdet(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                  scale = scale, ratio = ratio, octave = octave,
@@ -125,7 +141,9 @@ def effdet_d7x(x, n_class = 21,
                scale = [32, 64, 128, 256, 512, 1024], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
                neck = functools.partial(neck, n_feature = 384, n_sampling = 3, neck_n_depth = 8, neck = functools.partial(bifpn, n_feature = 384, weighted_add = False)),
                convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.swish):
-    #image_shape = [1536, 1536]
+    """
+    The recommended image shape is 1536.
+    """
     feature = effnet_b7(x, weights = weights, indices = [-3, -2, -1])
     out = effdet(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                  scale = scale, ratio = ratio, octave = octave,
@@ -146,7 +164,9 @@ def effdet_lite_d0(x, n_class = 21,
                    scale = [24, 48, 96, 192, 320], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
                    neck = functools.partial(neck, n_feature = 64, neck_n_depth = 3, neck = functools.partial(bifpn, n_feature = 64, weighted_add = False, activation = tf.nn.relu6)),
                    convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.relu6):
-    #image_shape = [320, 320]
+    """
+    The recommended image shape is 320.
+    """
     feature = effnet_lite_b0(x, weights = weights, indices = [-3, -2, -1])
     out = effdet_lite(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                       scale = scale, ratio = ratio, octave = octave,
@@ -158,7 +178,9 @@ def effdet_lite_d1(x, n_class = 21,
                    scale = [24, 48, 96, 192, 384], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
                    neck = functools.partial(neck, n_feature = 88, neck_n_depth = 4, neck = functools.partial(bifpn, n_feature = 88, weighted_add = False, activation = tf.nn.relu6)),
                    convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.relu6):
-    #image_shape = [384, 384]
+    """
+    The recommended image shape is 384.
+    """
     feature = effnet_lite_b1(x, weights = weights, indices = [-3, -2, -1])
     out = effdet_lite(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                       scale = scale, ratio = ratio, octave = octave,
@@ -170,7 +192,9 @@ def effdet_lite_d2(x, n_class = 21,
                    scale = [24, 48, 96, 192, 336], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
                    neck = functools.partial(neck, n_feature = 112, neck_n_depth = 5, neck = functools.partial(bifpn, n_feature = 112, weighted_add = False, activation = tf.nn.relu6)),
                    convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.relu6):
-    #image_shape = [448, 448]
+    """
+    The recommended image shape is 448.
+    """
     feature = effnet_lite_b2(x, weights = weights, indices = [-3, -2, -1])
     out = effdet_lite(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                       scale = scale, ratio = ratio, octave = octave,
@@ -182,7 +206,9 @@ def effdet_lite_d3(x, n_class = 21,
                    scale = [24, 48, 96, 192, 384], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
                    neck = functools.partial(neck, n_feature = 160, neck_n_depth = 6, neck = functools.partial(bifpn, n_feature = 160, weighted_add = False, activation = tf.nn.relu6)),
                    convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.relu6):
-    #image_shape = [512, 512]
+    """
+    The recommended image shape is 512.
+    """
     feature = effnet_lite_b3(x, weights = weights, indices = [-3, -2, -1])
     out = effdet_lite(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                       scale = scale, ratio = ratio, octave = octave,
@@ -194,7 +220,9 @@ def effdet_lite_d3x(x, n_class = 21,
                     scale = [24, 48, 96, 192, 384], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
                     neck = functools.partial(neck, n_feature = 200, neck_n_depth = 6, neck = functools.partial(bifpn, n_feature = 200, weighted_add = False, activation = tf.nn.relu6)),
                     convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.relu6):
-    #image_shape = [640, 640]
+    """
+    The recommended image shape is 640.
+    """
     feature = effnet_lite_b3(x, weights = weights, indices = [-3, -2, -1])
     out = effdet_lite(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                       scale = scale, ratio = ratio, octave = octave,
@@ -206,7 +234,9 @@ def effdet_lite_d4(x, n_class = 21,
                    scale = [32, 64, 128, 256, 512], ratio = [0.5, 1, 2], octave = 3, weights = "imagenet",
                    neck = functools.partial(neck, n_feature = 224, neck_n_depth = 7, neck = functools.partial(bifpn, n_feature = 224, weighted_add = False, activation = tf.nn.relu6)),
                    convolution = separable_conv, normalize = tf.keras.layers.BatchNormalization, activation = tf.nn.relu6):
-    #image_shape = [640, 640]
+    """
+    The recommended image shape is 640.
+    """
     feature = effnet_lite_b4(x, weights = weights, indices = [-3, -2, -1])
     out = effdet_lite(feature, n_class = n_class, image_shape = x, n_feature = n_feature, n_depth = n_depth,
                       scale = scale, ratio = ratio, octave = octave,
