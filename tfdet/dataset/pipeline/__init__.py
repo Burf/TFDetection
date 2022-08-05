c3bb147b03ed2316888f0b7d9fa08bd5d4e7268b
unknown <flslzk@gmail.com>
2022-08-05 10:47:57 +0900

    add base pipe

--- tfdet/dataset/pipeline/__init__.py
+++ tfdet/dataset/pipeline/__init__.py
@@ -1,6 +1,7 @@
 from .transform import load, preprocess, resize, pad, crop, random_crop, mosaic, cut_mix, albumentations
 from .formatting import key_map, collect
-from .pipe import (load_pipe, preprocess_pipe, 
+from .pipe import (custom_pipe,
+                   load_pipe, preprocess_pipe, 
                    resize_pipe, pad_pipe, crop_pipe, random_crop_pipe, 
                    mosaic_pipe, cut_mix_pipe, albumentations_pipe, 
                    key_map_pipe, collect_pipe)
--- tfdet/dataset/pipeline/pipe.py
+++ tfdet/dataset/pipeline/pipe.py
@@ -9,6 +9,39 @@ from .transform import load, preprocess, resize, pad, crop, random_crop, mosaic,
 from .formatting import key_map, collect
 from ..util import load_image, load_pascal_voc
 
+def custom_pipe(function, x_true, y_true = None, bbox_true = None, mask_true = None, dtype = None, tf_func = False,
+                pre_batch_size = 0, pre_shuffle = False, pre_shuffle_size = None, pre_prefetch = False, pre_prefetch_size = None,
+                batch_size = 0, epoch = 1, shuffle = False, prefetch = False, num_parallel_calls = None, cache = None, shuffle_size = None, prefetch_size = None,
+                **kwargs):
+    args = [arg for arg in [x_true, y_true, bbox_true, mask_true] if arg is not None]
+    args = args[0] if len(args) == 1 else tuple(args)
+    pre_pipe = pipeline(args, batch_size = pre_batch_size, shuffle = pre_shuffle, prefetch = pre_prefetch, num_parallel_calls = num_parallel_calls, shuffle_size = pre_shuffle_size, prefetch_size = pre_prefetch_size)
+    func = None
+    if tf_func:
+        func = functools.partial(function, **kwargs)
+    elif callable(function):
+        if dtype is None:
+            if isinstance(x_true, tf.data.Dataset):
+                dtype = tuple(pre_pipe.element_spec.values()) if isinstance(pre_pipe.element_spec, dict) else pre_pipe.element_spec
+            else:
+                if isinstance(x_true, dict):
+                    sample_result = function(**{k:v[0] for k, v in x_true.items()}, **kwargs)
+                else:
+                    sample_args = [v[0] for v in [x_true, y_true, bbox_true, mask_true] if v is not None]
+                    sample_result = function(*sample_args, **kwargs)
+                dtype = [tf.convert_to_tensor(r).dtype for r in (sample_result if isinstance(sample_result, tuple) else (sample_result,))]
+                dtype = dtype[0] if len(dtype) == 1 else tuple(dtype)
+        else:
+            if isinstance(x_true, tf.data.Dataset):
+                if isinstance(x_true.element_spec, tuple) or isinstance(x_true.element_spec, dict):
+                    dtype = [dtype] * len(x_true.element_spec)
+            else:
+                dtype = [dtype] * len(x_true if isinstance(x_true, dict) else [arg for arg in [x_true, y_true, bbox_true, mask_true] if arg is not None])
+                dtype = dtype[0] if len(dtype) == 1 else tuple(dtype)
+        func = functools.partial(py_func, function, Tout = dtype, **kwargs) if callable(function) else None
+    pipe = pipeline(pre_pipe, map = func, batch_size = batch_size, epoch = epoch, shuffle = shuffle, prefetch = prefetch, num_parallel_calls = num_parallel_calls, cache = cache, shuffle_size = shuffle_size, prefetch_size = prefetch_size)
+    return pipe
+
 def load_pipe(x_true, y_true = None, bbox_true = None, mask_true = None, load_func = load_image, anno_func = load_pascal_voc,
               batch_size = 0, epoch = 1, shuffle = False, prefetch = False, num_parallel_calls = None, cache = None, shuffle_size = None, prefetch_size = None):
     if isinstance(x_true, dict):
